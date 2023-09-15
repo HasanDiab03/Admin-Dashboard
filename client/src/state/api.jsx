6b7f0c3ea3592081a3f03b6d0e92308d2677067e
Hasan Diab <hasandiab.main@gmail.com>
2023-09-15 17:46:07 +0300

    deployed url

--- client/src/state/api.jsx
+++ client/src/state/api.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://admin-dasboard-backend.onrender.com",
   }),
   reducerPath: "adminApi",
   tagTypes: [
